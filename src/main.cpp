91868eeb9d0342ec16d274496e23f0bc3ea14a8e
Richard Riley <rileyrg@gmail.com>
2019-09-17 22:22:37 +0200

    segmentToPort

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,32 @@
 #include<Arduino.h>
 
-int a[10][10]={
-               {0,0,0,1,1,1,0,1,1,1},  //0
-               {0,0,0,1,0,0,0,0,0,1},  //1
-               {0,0,0,0,1,1,1,0,1,1},  //2
-               {0,0,0,1,1,0,1,0,1,1},  //3
-               {0,0,0,1,0,0,1,1,0,1},  //4
-               {0,0,0,1,1,0,1,1,1,0},  //5
-               {0,0,0,1,1,1,1,1,1,0},  //6
-               {0,0,0,1,0,0,0,0,1,1},  //7
-               {0,0,0,1,1,1,1,1,1,1},  //8
-               {0,0,0,1,1,0,1,1,1,1},};//9  
+int segmentToPort[]={3,4,5,6,7,8,9};
+
+bool ledBits[10][7]={
+     {1,1,1,0,1,1,1},  //0
+     {1,0,0,0,0,0,1},  //1
+     {0,1,1,1,0,1,1},  //2
+     {1,1,0,1,0,1,1},  //3
+     {1,0,0,1,1,0,1},  //4
+     {1,1,0,1,1,1,0},  //5
+     {1,1,1,1,1,1,0},  //6
+     {1,0,0,0,0,1,1},  //7
+     {1,1,1,1,1,1,1},  //8
+     {1,1,0,1,1,1,1},};//9
+
 void setup(){  
-  for (int i=3;i<=9;i++) {  
-    pinMode(i,OUTPUT);  
-  }  
+     for (int i=0;i<=sizeof(segmentToPort);i++) {  
+          pinMode(segmentToPort[i],OUTPUT);  
+     }  
 }  
-void printf(int v){  
-  for (int i=3;i<=9;i++){  
-    digitalWrite(i,a[v][i]);  
-  }  
+void printLEDDigit(int v){  
+     for (int i=0;i<=sizeof(segmentToPort);i++) {  
+          digitalWrite(segmentToPort[i],ledBits[v][i]);  
+     }  
 }  
 void loop(){  
-  for (int i=0;i<=9;i++){  
-    printf(i);  
-    delay(400);  
-  }  
+     for (int i=0;i<=9;i++){  
+          printLEDDigit(i);  
+          delay(400);  
+     }  
 }
